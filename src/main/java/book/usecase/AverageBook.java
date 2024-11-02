f5d3a17c605153df2a2df4f54df0f5acb9602218
MinhQuang-dev <phanquang704@gmail.com>
2024-11-03 05:53:30 +0700

    update source code

--- src/main/java/book/usecase/AverageBook.java
+++ src/main/java/book/usecase/AverageBook.java
@@ -8,7 +8,7 @@ class AverageBook {
     public double calculateAverage(List<Book> books) {
         double sum = 0;
         for (Book book : books) {
-            sum += book.thanhTien(); // Assuming thanhTien() calculates the price
+            sum += book.thanhTien(); 
         }
         return books.isEmpty() ? 0 : sum / books.size();
     }
